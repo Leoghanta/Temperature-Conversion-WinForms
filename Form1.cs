13820085854eb87b7b0ac6fdb7a968063fab8389
Jimi Wilson <t.wilson@dundeeandangus.ac.uk>
2024-01-17 09:55:20 +0000

    Added Comments

--- Form1.cs
+++ Form1.cs
@@ -12,19 +12,29 @@ namespace Temperature_Conversion
 {
     public partial class Form1 : Form
     {
+        /// <summary>
+        /// Instantise the temperature conversion object
+        /// </summary>
         TemperatureConversion tempconvert = new TemperatureConversion();
 
+        /// <summary>
+        /// Initialise the Form
+        /// </summary>
         public Form1()
         {
             InitializeComponent();
             //Set up the data bindings.
             //new binding ("property changing", object, "object property", formatting (false), data source update mode)
             //}
+            InitBindings();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        /// <summary>
+        /// Initialise the bindings for the text boxes.
+        /// This will listen for a property change and update themselves.
+        /// </summary>
+        private void InitBindings()
         {
-
             CelsiusTextBox.DataBindings.Add(new Binding("Text", tempconvert, "Celsius", false, DataSourceUpdateMode.OnPropertyChanged));
             FahrenheitTextBox.DataBindings.Add(new Binding("Text", tempconvert, "Fahrenheit", false, DataSourceUpdateMode.OnPropertyChanged));
             KelvinTextBox.DataBindings.Add(new Binding("Text", tempconvert, "Kelvin", false, DataSourceUpdateMode.OnPropertyChanged));
--- TemperatureConversion.cs
+++ TemperatureConversion.cs
@@ -9,6 +9,9 @@ namespace Temperature_Conversion
 {
     internal class TemperatureConversion: INotifyPropertyChanged
     {
+        /// <summary>
+        /// Variables for Celsius Temperature
+        /// </summary>
         private double _celsius = 0;
         public double Celsius
         {
@@ -20,6 +23,9 @@ namespace Temperature_Conversion
             }
         }
 
+        /// <summary>
+        /// Temperature for Fahrenheit Temperatures
+        /// </summary>
         private double _fahrenheit = 0;
         public double Fahrenheit
         {
@@ -31,6 +37,9 @@ namespace Temperature_Conversion
             }
         }
 
+        /// <summary>
+        /// Variables for Kelvin Temperatures
+        /// </summary>
         private double _kelvin = 0;
         public double Kelvin
         {
@@ -44,27 +53,42 @@ namespace Temperature_Conversion
         }
 
 
+        /// <summary>
+        /// Calculate fahrenheit and kelvin when Celsius has changed
+        /// </summary>
         public void ConvertCelsius()
         {
             _fahrenheit =  Math.Round((Celsius * 9 / 5) + 32, 2);
             _kelvin = Math.Round(Celsius + 273.15);
         }
 
+        /// <summary>
+        /// Calculate Celsius and Kelvin when Fahrenheit has changed
+        /// </summary>
         public void ConvertFahrenheit()
         {
             _celsius = Math.Round((Fahrenheit - 32) * 5 / 9, 2) ;
             _kelvin = Math.Round((Fahrenheit - 32) * 5 / 9 + 273.15);
         }
 
+        /// <summary>
+        /// Calculate Fahrenheit and Celsius when Kelvin has changed
+        /// </summary>
         public void ConvertKelvin()
         {
             _fahrenheit = (Kelvin - 273.15) * 9 / 5 + 32;
             _celsius = Kelvin - 273.15;
         }
 
-
+        /// <summary>
+        /// Create event handler for property changed.
+        /// </summary>
         public event PropertyChangedEventHandler PropertyChanged;
 
+        /// <summary>
+        /// When a property has changed, Broadcast this to the whole program. 
+        /// </summary>
+        /// <param name="property">String name of the property that changed.</param>
         public void OnPropertyChanged(string property)
         {
             PropertyChanged.Invoke(this, new PropertyChangedEventArgs(property));
